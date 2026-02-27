113de4e43d658e04e29a7915d1a93b833d91d6bd
Junior <94208651+Junior4144@users.noreply.github.com>
2026-02-26 20:32:14 -0600

    adds authorization / ability to accept login/register calls

--- Tracksesh.API/Tracksesh/Program.cs
+++ Tracksesh.API/Tracksesh/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Tracksesh.Data;
 using Tracksesh.Repositories.Implementation;
 using Tracksesh.Repositories.Interface;
@@ -16,6 +19,51 @@ builder.Services.AddDbContext<AuthDbContext>(options =>
 });
 
 builder.Services.AddScoped<ITokenRepository, TokenRepository>();
+
+builder.Services.AddIdentityCore<IdentityUser>()
+    .AddRoles<IdentityRole>()
+    .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("Tracksesh")
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            AuthenticationType = "Jwt",
+            ValidateIssuer = false,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey =
+            new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                if (context.Request.Cookies.TryGetValue("access_token", out var token))
+                {
+                    context.Token = token;
+                }
+                return Task.CompletedTask;
+            }
+        };
+    });
 
 var app = builder.Build();
 
--- Tracksesh.API/Tracksesh/obj/Debug/net10.0/Tracksesh.AssemblyInfo.cs
+++ Tracksesh.API/Tracksesh/obj/Debug/net10.0/Tracksesh.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tracksesh")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0df97651122a31a7552a16736d1f8a2a4402d63f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e3fb7a06fcb582864fc3cbd7fb99c4059362c06")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tracksesh")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tracksesh")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
